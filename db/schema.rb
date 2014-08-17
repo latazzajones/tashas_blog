66af0faaab9200370b16d66ccd2a1797dc23251c
natasha jones <leo@natasha-joness-MacBook-Pro.local>
2014-08-17 17:13:07 -0400

    Switch to Postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140809000030) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "body"
@@ -33,7 +36,7 @@ ActiveRecord::Schema.define(version: 20140809000030) do
     t.datetime "updated_at"
   end
 
-  add_index "authors", ["email"], name: "index_authors_on_email", unique: true
+  add_index "authors", ["email"], name: "index_authors_on_email", unique: true, using: :btree
 
   create_table "comments", force: true do |t|
     t.string   "author_name"
@@ -43,7 +46,7 @@ ActiveRecord::Schema.define(version: 20140809000030) do
     t.datetime "updated_at"
   end
 
-  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
 
   create_table "taggings", force: true do |t|
     t.integer  "tag_id"
@@ -52,7 +55,7 @@ ActiveRecord::Schema.define(version: 20140809000030) do
     t.datetime "updated_at"
   end
 
-  add_index "taggings", ["article_id"], name: "index_taggings_on_article_id"
+  add_index "taggings", ["article_id"], name: "index_taggings_on_article_id", using: :btree
 
   create_table "tags", force: true do |t|
     t.string   "name"
