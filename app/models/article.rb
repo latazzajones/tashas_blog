b5678a8b0351c9c7b9f8e86d8d21a8a8d7c58da2
Natasha Jones <tasha.k.jones@gmail.com>
2014-10-03 17:06:43 -0400

    replace ".image.exist?" with ".image_url"

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,8 +4,8 @@ class Article < ActiveRecord::Base
 	has_many :comments
 	has_many :taggings
 	has_many :tags, through: :taggings
-	has_attached_file :image
-	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/gif", "image/png"]
+	# has_attached_file :image
+	# validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/gif", "image/png"]
 
 	def total_pages
 		@articles = Article.all
