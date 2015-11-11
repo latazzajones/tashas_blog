601aa7d7503b45107b1811787fdc4f94f05db052
Natasha Jones <tasha.k.jones@gmail.com>
2015-11-10 20:07:35 -0500

    start reimagining

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get "static_pages/about"
-  get "static_pages/reading_list"
   root to: 'articles#index'
   resources :articles do
     resources :comments
@@ -9,8 +7,11 @@ Rails.application.routes.draw do
   resources :authors
 
   resources :author_sessions, only: [ :new, :create, :destroy ]
+    get 'login' => 'author_sessions#new'
+    get 'logout' => 'author_sessions#destroy'
 
-  get 'login' => 'author_sessions#new'
-  get 'logout' => 'author_sessions#destroy'
+  #static 
+  get "static_pages/about"
+  get "static_pages/reading_list"
 
 end
